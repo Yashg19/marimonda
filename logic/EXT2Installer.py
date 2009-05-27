a86822eb7d57a9c4b2085b64593168d7662e7d48
yamil.llanos <yamil.llanos@ac6e9e1e-4a12-11de-82d9-516ade3e46db>
2009-05-27 17:23:59 +0000

    New EXT2Installer and EXT2InstallerSettings classes

---
+++ logic/EXT2Installer.py
@@ -0,0 +1,17 @@
+'''
+Created on 27/05/2009
+
+@author: yllanos
+'''
+
+class EXT2Installer():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
---
+++ logic/EXT2InstallerSettings.py
@@ -0,0 +1,24 @@
+'''
+Created on 26/05/2009
+
+@author: yllanos
+'''
+
+class EXT2InstallerSettings():
+    '''
+    Settings class for the EXT2 Filesystem
+    '''
+    partitionSize = 21474836480 #20GB
+    blockSize = 2048 #2048 bytes block
+    nBlockGroups = int(partitionSize/(8*blockSize*blockSize))
+    mean = 1024     #Mean file size
+    stdDev = 3.2    #Standard deviation
+    blocksPerGroup = 8*blockSize
+    inodesPerGroup = 8*blocksPerGroup
+    nOperations = 300000
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
