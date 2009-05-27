6aeb65581ee4d4a087dbd451958fbb0e280ae9c6
yamil.llanos <yamil.llanos@ac6e9e1e-4a12-11de-82d9-516ade3e46db>
2009-05-27 18:56:24 +0000

    EXT2Installer class imports EXT2InstallerSettings

--- trunk/logic/EXT2Installer.py
+++ trunk/logic/EXT2Installer.py
@@ -4,11 +4,14 @@ Created on 27/05/2009
 @author: yllanos
 '''
 
+from EXTInstallerSettings import *
+
 class EXT2Installer():
     '''
     classdocs
     '''
 
+    osSettings = EXTInstallerSettings()
 
     def __init__(selfparams):
         '''
