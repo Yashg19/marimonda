ff274dda42f0e9e4c9be4413fd023e2065931c9d
yamil.llanos <yamil.llanos@ac6e9e1e-4a12-11de-82d9-516ade3e46db>
2009-06-30 14:15:28 +0000

    EXT2: reordering of some members; create_file() prototype.  EXT2BlockGroup: create_inode() calculates the number of blocks needed for a file, fills the other members and finally returns an almost complete Inode object; get_blocks() continues as an on-going effort, it is supported by several methods of name fill_*; 2nd and 3rd indirections mechanisms are pending.  All the variations of find_block() are also pending

--- trunk/logic/EXT2.py
+++ trunk/logic/EXT2.py
@@ -23,6 +23,7 @@ class EXT2(Filesystem):
     partitionSize = osSettings.partitionSize
     blockSize = osSettings.blockSize
     nBlockGroups = osSettings.nBlockGroups    #Number of block groups
+    currentGroup = 0
     
 
     def __init__(selfparams):
@@ -32,33 +33,47 @@ class EXT2(Filesystem):
         
     def create_file(self, fileSize):
         '''
-        
+        This method creates a file given a file size.
+        It search for a group and when available, creates 
+        the the inode and its corresponding blocks
         '''
-        group = find_group()
-        if group != None:
-            pass
-        else:
+        if find_group() == True:
+            inode = blockGroups[currentGroup].create_inode(filesize)
+            inode.i_inode = inode.i_inode + currentGroup*osSettings.inodesPerGroup
+            blockGroups[currentGroup] = inode
+            blockGroups[currentGroup].groupDescriptor.bgFreeBlockCount = blockGroups[currentGroup].groupDescriptor.bgFreeBlockCount - 1
+            blockGroups[currentGroup].groupDescriptor.bgFreeInodesCount = blockGroups[currentGroup].groupDescriptor.bgFreeInodesCount - 1
+            blockGroups[currentGroup].superBlocks_inodes_count = blockGroups[currentGroup].superBlocks_inodes_count + 1
+            blockGroups[currentGroup].s_free_inodes_count = blockGroups[currentGroup].s_free_inodes_count + 1 
+        else:   #Means the partition is full!
             pass
         return None
         
     def delete_file(self, inode):
+    #From the inode number, it should be possible to compute its group,
+    #go there and delete the inode using the ceil() method.
+    #For the moment, the proposed idea would be to use random.choice on the
+    #keys of blockGroups to pick a random inode on a random group
         '''
-        
+        This method receives the inode number,
+        calculates the group it belongs to, and
+        deletes it from the file system
         '''        
         return None
 
-    def find_group(self):
+    def find_group(self, currentGroup):
         '''
-        Search the whole file system 
+        Search the whole file system
         for a block group with free blocks
         '''
-        if len(blockGroups.keys())==0:  #Means the file system is empty, so start on block group 0
-            return 0
-            '''
-            blockGroups = {0:EXT2BlockGroup()}
-            findBlockResult = blockGroups[0].find_block()
-            if findBlockResult[0] == True:
-                blockGroups[0].InodeTable[0] = blockGroups[0].create_inode()
-            '''
+        if blockGroups.__len__()==0:  #Means the file system is empty, so start on block group 0
+            blockGroups = {0:EXT2BlockGroup()}  #Create the block group 
+            currentGroup = 0 #blockGroups[0].find_block()
+            return True
+            #blockGroups[0].InodeTable[0] = blockGroups[0].create_inode()
+            
         else:
-            pass        
+            if blockGroups[currentGroup].groupDescriptor.bgFreeBlocksCount > 0:
+                return True
+            else:   #The current group is full
+                return False
