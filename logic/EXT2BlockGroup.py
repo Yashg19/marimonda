e498ae1b24143db83bcdf5150432f77a1d2b6e5d
yamil.llanos <yamil.llanos@ac6e9e1e-4a12-11de-82d9-516ade3e46db>
2009-06-30 14:15:28 +0000

    EXT2: reordering of some members; create_file() prototype.  EXT2BlockGroup: create_inode() calculates the number of blocks needed for a file, fills the other members and finally returns an almost complete Inode object; get_blocks() continues as an on-going effort, it is supported by several methods of name fill_*; 2nd and 3rd indirections mechanisms are pending.  All the variations of find_block() are also pending

--- logic/EXT2BlockGroup.py
+++ logic/EXT2BlockGroup.py
@@ -16,9 +16,9 @@ class EXT2BlockGroup():
     This class represents a single Block Group
     '''
 
-    InodeTable = {}
-    dataBlocksBitmap = []
-    inodeBitmap = []
+    inodeTable = {}
+    inodeBitmap = {}
+    dataBlocksBitmap = {}
     groupDescriptor = EXT2GroupDescriptor()
     groupDescriptor.bgFreeBlocksCount = osSettings.nBlockGroups
     groupDescriptor.bgFreeInodesCount = osSettings.inodesPerGroup
@@ -28,20 +28,52 @@ class EXT2BlockGroup():
         Constructor
         '''
         
-    def create_inode(self):
+    def create_inode(self, filesize):     #Debería crear los bloques del archivo de una vez
         '''
-        
+        This method creates the inode for every file and
+        also creates the associated blocks by calling the
+        get_blocks() method.
+        It returns an Inode object.
         '''
-        return Inode()        
+        newInode = Inode()
+        if inodeTable.__len__() > 0:
+            newInode.i_inode = inodeTable.__len__() + 1
+        else:
+            newInode.i_inode = 0
+        newInode.i_isize = filesize
+        if filesize < osSettings.blockSize:
+            blocksNeeded = 1
+        elif (filesize % osSettings.blockSize) > 0:
+            blocksNeeded = int(filesize/osSettings.blockSize) + 1
+        elif (filesize % osSettings.blockSize) == 0:
+            blocksNeeded = int(filesize/osSettings.blockSize)
+        newInode.i_iblock = get_blocks(blocksNeeded)
+        return newInode
+
+    def fill_direct_blocks(self, blocksNeeded):
+        '''
+
+        '''
+        i = 1
+        directBlocksList = []
+        while i <= blocksNeeded:
+            directBlocksList.append(find_block())
+            i = i + 1
+        return directBlocksList
+
+    def fill_first_indirect_blocks(self,blocksNeeded):
+        '''
+        '''
+        i = 1
+        firstIndirectList = []
+        firstIndirectList.append(fill_direct_blocks(blocksNeeded))
+        return firstIndirectList
         
     def find_block(self):
         '''
         
         '''
-        if 1>2:
-            return None
-        else:
-            return False
+        return None
     
     def find_block_prealloc(self):
         '''
@@ -54,4 +86,26 @@ class EXT2BlockGroup():
         
         '''
         return None
-        
+
+    def get_blocks(self,blocksNeeded):
+        '''
+        Calculates and creates the number of needed inodes for a file,
+        including both the direct and indirect ones.
+        It returns a list with the blocks.
+        '''
+        firstIndirection = (osSettins.blockSize/4) + 11
+        secondIndirection = ((osSettings.blockSize/4)*(osSettings.blockSize/4)) + firstIndirection
+        thirdIndirection = ((osSettings.blockSize/4)*(osSettings.blockSize/4)*(osSettings.blockSize/4)) + secondIndirection
+        if blocksNeeded <= 12:  #Direct addressing
+            return fill_direct_blocks(blocksNeeded)
+        else:
+            blocksList = []
+            blocksList.append(fill_direct_blocks(blocksNeeded))
+            if blocksNeeded > 12 and blocksNeeded <= firstIndirection #1st-Indirection
+                blocksList.append(fill_first_indirect_blocks(blocksNeeded-12))
+                return blocksList
+            else:
+                if blocksNeeded > firstIndirection + 1 and blocksNeeded <= secondIndirection   #2nd-Indirection
+                    pass
+                if blocksNeeded > secondIndirection + 1 and blocksNeeded <= thirdIndirection  #3rd-Indirection
+                    pass
