a40e4569c85a35878a9ed0c44a237699ff23fedb
yamil.llanos <yamil.llanos@ac6e9e1e-4a12-11de-82d9-516ade3e46db>
2009-07-01 13:34:18 +0000

    EXT2BlockGroup: 1st and 2nd level indirections implemented in theory, needs testing.  3rd-level indirection and find_block() method are still pending

--- logic/EXT2BlockGroup.py
+++ logic/EXT2BlockGroup.py
@@ -10,6 +10,7 @@ Created on 24/05/2009
 
 from Inode import *
 from EXT2 import *
+from math import *
 
 class EXT2BlockGroup():
     '''
@@ -52,7 +53,9 @@ class EXT2BlockGroup():
 
     def fill_direct_blocks(self, blocksNeeded):
         '''
-
+        This method fills a list with the blocks
+        directly addressed by the file system.
+        Usually addresses blocks 1 to 12.
         '''
         i = 1
         directBlocksList = []
@@ -63,12 +66,32 @@ class EXT2BlockGroup():
 
     def fill_first_indirect_blocks(self,blocksNeeded):
         '''
+        This method fills both the direct and indirect blocks and
+        returns a hierarchical list representing the 1st-level redirection
         '''
-        i = 1
         firstIndirectList = []
         firstIndirectList.append(fill_direct_blocks(blocksNeeded))
         return firstIndirectList
-        
+
+    def fill_second_indirect_blocks(self,blocksNeeded):
+        '''
+        Similar to fill_first_indirect_blocks(), but on a higher,
+        deeper, hierarchical level.
+        '''
+        secondIndirectList = []
+        firstIndirectSets = int(ceil(blocksNeeded/12))  #to compute how many 1st-indirect sets of blocks in groups of 12 we need
+        if blocksNedded > 12:
+            i = 1
+            while i <= firstIndirectSets:
+                firstIndirectList = []
+                secondIndirectList.append(firstIndirectList.append(fill_first_indirect_blocks(12)))
+                i = i + 1
+            remaining = blocksNeeded - (12*firstIndirectSets)
+            secondIndirectList.append(firstIndirectList.append(fill_first_indirect_blocks(remaining)))
+            return secondIndirectList
+        else:
+            return secondIndirectList.append(firstIndirectList.append(fill_first_indirect_blocks(blocksNeeded)))
+
     def find_block(self):
         '''
         
@@ -105,7 +128,9 @@ class EXT2BlockGroup():
                 blocksList.append(fill_first_indirect_blocks(blocksNeeded-12))
                 return blocksList
             else:
-                if blocksNeeded > firstIndirection + 1 and blocksNeeded <= secondIndirection   #2nd-Indirection
-                    pass
-                if blocksNeeded > secondIndirection + 1 and blocksNeeded <= thirdIndirection  #3rd-Indirection
+                if blocksNeeded > firstIndirection and blocksNeeded <= secondIndirection   #2nd-Indirection
+                    blocksList.append(fill_first_indirect_blocks(blocksNeeded-12))
+                    blocksList.append(fill_second_indirect_blocks(blocksNeeded - firstIndirection))
+                    return blocksList
+                if blocksNeeded > secondIndirection and blocksNeeded <= thirdIndirection  #3rd-Indirection
                     pass
